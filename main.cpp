a98a6776cd2d3d355789a39fad9e4d71d29b79f7
krtx <akimonoiryou@gmail.com>
2012-12-08 08:58:55 +0900

    class SVM

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 double train_svm(Matrix<double> x,
                  Vector<double> y,
                  Vector<double>& alpha,
-                 Kernel* kernel = new DotProd())
+                 Kernel* kernel)
 {
   int r = x.nrows();
   int n = r, m = r, p = 1;
@@ -37,6 +37,63 @@ double train_svm(Matrix<double> x,
   return solve_quadprog(G, g0, CE, ce0, CI, ci0, alpha);
 };
 
+class SVM {
+public:
+  SVM(Matrix<double> x, Vector<double> y, Kernel* kernel = new DotProd()):kernel(kernel), x(x), y(y) {
+    int r = x.nrows();
+    int n = r, m = r, p = 1;
+    Matrix<double> G(n, m), CE(n, p), CI(n, m);
+    Vector<double> g0(n), ce0(p), ci0(m);
+  
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++) {
+        G[i][j] = (*kernel)(x.extractRow(i), x.extractRow(j)) * y[i] * y[j];
+        if (i == j) G[i][j] += 1.9e-9;
+      }
+    for (int i = 0; i < n; i++) g0[i] = -1.0;
+    for (int i = 0; i < n; i++) CE[i][0] = y[i];
+    ce0[0] = 0.0;
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        CI[i][j] = i == j ? 1.0 : 0.0;
+    for (int i = 0; i < m; i++) ci0[i] = 0.0;
+
+    alpha.resize(n);
+
+    try {
+      solve_quadprog(G, g0, CE, ce0, CI, ci0, alpha);
+    } catch (const std::exception& ex) {
+      std::cerr << "lerning failed: " << ex.what() << std::endl;
+      throw;
+    }
+
+    theta = sum(x.extractRow(0)) - y[0];
+  };
+
+  void dump_alpha() {
+    for (int i = 0; i < alpha.size(); i++)
+      printf("alpha[%d] = %.6f\n", i, alpha[i]);
+  };
+
+  double discriminant(Vector<double> v) {
+    return sum(v) - theta;
+  };
+
+private:
+  Kernel* kernel;
+  Vector<double> alpha;
+  double theta;
+  Matrix<double> x;
+  Vector<double> y;
+
+  double sum(Vector<double> v) {
+    double ret = 0.0;
+    for (int i = 0; i < x.ncols(); i++)
+      ret += alpha[i] * y[i] * (*kernel)(v, x.extractRow(i));
+    return ret;
+  }
+};
+
 void read_input(std::istream& ifs, Matrix<double>& x, Vector<double>& y)
 {
   std::vector<std::vector<double> > _x;
@@ -49,6 +106,8 @@ void read_input(std::istream& ifs, Matrix<double>& x, Vector<double>& y)
     
     while (is >> val) v.push_back(val);
     _y.push_back(v.back()); v.pop_back();
+
+    // remove duplicates
     if (find(_x.begin(), _x.end(), v) == _x.end())
       _x.push_back(v);
     else
@@ -73,26 +132,10 @@ int main(int argc, char *const argv[])
   Vector<double> y;
   read_input(ifs, x, y);
 
-  Vector<double> alpha;
+  //Gaussian *g = new Gaussian(10.0);
+  SVM svm(x, y);
 
-  train_svm(x, y, alpha);
-
-  for (int i = 0; i < alpha.size(); i++)
-    printf("alpha[%d] = %.6f\n", i, alpha[i]);
-
-  Vector<double> weight(0.0, x.ncols());
-  for(int i = 0; i < alpha.size(); i++)
-    weight += alpha[i] * y[i] * x.extractRow(i);
-
-  double theta = dot_prod(weight, x.extractRow(0)) - y[0];
-
-  Vector<double> a(x.ncols());
-  a[0] = 0.0; a[1] = 0.0;
-  printf("%f\n", dot_prod(weight, a) - theta);
-  a[0] = 20.0; a[1] = 20.0;
-  printf("%f\n", dot_prod(weight, a) - theta);
-  a[0] = 10.0; a[1] = 10.0;
-  printf("%f\n", dot_prod(weight, a) - theta);
+  svm.dump_alpha();
 
   return 0;
 }
